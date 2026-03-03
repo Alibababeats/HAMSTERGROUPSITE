b9e12ec30984e7c835494a10f568a040b7327c1e
Alibababeats <ali.salem.03.15@gmail.com>
2026-03-02 22:50:04 -0500

    production deployment to vercel

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        personas: resolve(__dirname, 'personas.html'),
+      },
+    },
+  },
+});
